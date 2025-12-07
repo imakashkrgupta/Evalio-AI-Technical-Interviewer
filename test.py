646a1388f7e7510984d2cec327eb89b924a326b0
Akash Kumar Gupta <96005719+imakashkrgupta@users.noreply.github.com>
2025-12-07 17:43:54 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import ollama
-from ollama import chat
-from ollama import ChatResponse
-
-response = ollama.generate(model='llama3.2:1b', prompt='Why is the sky blue?')
-
-print(response.message.content)
